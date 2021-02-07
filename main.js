b0063879342a59e6ec3fa41e5904abc2de1ea646
Tomasz Biezunski <t.biezunski@icloud.com>
2021-02-07 09:05:35 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Tomasz';
+const age = 41;
+
+
+alert('Trzeci dział nauki frontendu');
+console.log(firstName);
+console.log(age);
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lata`);
